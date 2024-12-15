7b686674c91c4c7e9f6230b3a2f826d1bca945c7
Akshay Panchivala <akshaypanchivala@gmail.com>
2024-12-14 23:07:43 -0800

    added package

--- src/testGenerator.js
+++ src/testGenerator.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra');
 const babelParser = require('@babel/parser');
 const jestTemplate = require('../templates/jestTemplate');
-const mochaTemplate = require('../templates/mochaTemplate');
+// const mochaTemplate = require('../templates/mochaTemplate');
 
 async function generateTests(path, framework) {
   const code = await fs.readFile(path, 'utf-8');
@@ -11,7 +11,9 @@ async function generateTests(path, framework) {
   if (framework === 'jest') {
     testCases = jestTemplate.generateTestCases(ast);
   } else {
-    testCases = mochaTemplate.generateTestCases(ast);
+    testCases = jestTemplate.generateTestCases(ast);
+
+    // testCases = mochaTemplate.generateTestCases(ast);
   }
 
   const testFilePath = path.replace(/\.js$/, `.test.js`);
